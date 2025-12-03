5e21294087132b24f5bf790543c559e2ee8ef45a
Diesel127 <retina127127@gmail.com>
2025-12-03 16:10:47 +0100

    lesson24

--- PythonProject3/main.py
+++ PythonProject3/main.py
@@ -1,41 +1,34 @@
-import poplib
-from email.parser import BytesParser
-from email.policy import default
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-# Учётные данные для входа
-username = 'yahoo.com'
-password = 'password'
+# Настройки
+smtp_server = 'smtp.example.com'
+smtp_port = 587
+username = 'your_email@example.com'
+password = 'your_password'
+from_email = 'your_email@example.com'
+to_email = 'recipient@example.com'
+subject = 'Тема письма'
+body = 'Это тело письма'
 
-# Подключение к почтовому серверу Gmail
-pop3_server = 'pop.mail.yahoo.com'
-mailbox = poplib.POP3_SSL(pop3_server, 995)
+# Создание сообщения
+msg = MIMEMultipart()
+msg['From'] = from_email
+msg['To'] = to_email
+msg['Subject'] = subject
+msg.attach(MIMEText(body, 'plain'))
+server = None
 
-# Вход в почтовый ящик
-mailbox.user(username)
-mailbox.pass_(password)  # Используем pass_ для предотвращения конфликта с ключевым словом pass в Python
-
-num_messages = len(mailbox.list()[1])
-
-# Если есть письма, получаем содержимое последнего
-if num_messages > 0:
-    response, lines, octets = mailbox.retr(num_messages)
-    message_content = b'\r\n'.join(lines)
-
-    # Парсинг письма
-    message = BytesParser(policy=default).parsebytes(message_content)
-
-    # Отображение содержимого последнего письма
-    print(f"Subject: {message['subject']}")
-    print(f"From: {message['from']}")
-    print(f"To: {message['to']}")
-    print(f"Date: {message['date']}")
-    print("\nBody:\n")
-    if message.is_multipart():
-        for part in message.iter_parts():
-            if part.get_content_type() == "text/plain":
-                print(part.get_payload(decode=True).decode(part.get_content_charset()))
-    else:
-        print(message.get_payload(decode=True).decode(message.get_content_charset()))
-
-# Закрытие соединения
-mailbox.quit()
+# Подключение к SMTP-серверу и отправка письма
+try:
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()  # Установим соединение TLS
+    server.login(username, password)
+    server.sendmail(from_email, to_email, msg.as_string())
+    print("Письмо успешно отправлено")
+except Exception as e:
+    print(f"Ошибка при отправке письма: {e}")
+finally:
+    if server:
+        server.quit()
