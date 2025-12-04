a106ad8f70c2fdbf39b909f0e9a0a6502a8c40f1
Diesel127 <retina127127@gmail.com>
2025-12-04 15:08:39 +0100

    lesson27

--- PythonProject3/main.py
+++ PythonProject3/main.py
@@ -1,14 +1,13 @@
 import requests
 
 API_KEY = "API key"
-address = "Zitna 42 Prague"
-url = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={API_KEY}'
+city = input("Введите название города")
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
 response = requests.get(url)
-data = response.json()
-if data['status'] == 'OK':
-    location = data['results'][0]['geometry']['location']
-    latitude = location['lat']
-    londitute = location['lng']
-    print(f"Coordinates for '{address}': Latitude = {latitude}, Longitude = {longitude}")
+
+if response.status_code == 200:
+        data = response.json()
+        print(f"Current temperature in {city}: {data['main']['temp']}°C")
+        print(f"Weather description: {data['weather'][0]['description']}")
 else:
-        print(f"Error: {data['status']}")
+        print(f"Error: {response.status_code} - {response.json()['message']}")
