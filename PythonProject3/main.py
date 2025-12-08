a029013b6b34dc338ab9bf83afde453338953c81
Diesel127 <retina127127@gmail.com>
2025-12-08 14:41:12 +0100

    lesson29

--- PythonProject3/main.py
+++ PythonProject3/main.py
@@ -1,3 +1,10 @@
-my_list = [5, 0, 9, 8 ,7 , 5, 3, 2, 1, 0, 5]
-mpv = max(set(my_list), key=my_list.count)
-print(mpv)
+import threading
+
+
+def greet(name):
+    print(f"Hello, {name}")
+
+
+t = threading.Timer(5.0, greet, args=["Alice"])
+t.start()
+t.cancel()
