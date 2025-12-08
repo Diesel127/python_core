9e5559933bb3298158ca918fe6ddcd53265be038
Diesel127 <retina127127@gmail.com>
2025-12-08 15:48:01 +0100

    lesson32

--- PythonProject3/main.py
+++ PythonProject3/main.py
@@ -1,19 +1,26 @@
-import threading
+import asyncio
 
-user_data = threading.local()
 
-def process_data(user):
-    user_data.user = user
-    greet(user)
-def greet(user):
-    print(f"Hello, {user}, you data are processed")
+async def say_hello(t, msg):
+    await asyncio.sleep(t)
+    print(msg, end="")
+    await say_name(2, "Denis")
 
-threads = []
 
-users = ["Alice", "Bob", "Ben"]
-for user in users:
-    t = threading.Thread(target=process_data, args = (user,))
-    threads.append(t)
-    t.start()
-for t in threads:
-    t.join()
+async def say_name(t, name):
+    await asyncio.sleep(t)
+    print(name)
+    await say_end("END")
+
+
+async def say_end(msg):
+    print(msg)
+
+
+async def main():
+    task1 = asyncio.create_task(say_hello(1, "Hello, "))
+
+    await task1
+
+
+asyncio.run(main())
