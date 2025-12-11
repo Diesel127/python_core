d3b3d9c0b620515a2742e4b1200c74f0a0043ce0
Diesel127 <retina127127@gmail.com>
2025-12-11 13:57:02 +0100

    lesson36

--- PythonProject3/main.py
+++ PythonProject3/main.py
@@ -1,13 +1,15 @@
 import asyncio
 
-async def task_1():
-    print("Первая задача")
-    await asyncio.sleep(2)
-async def task_2():
-    print("Вторая задача")
-    await asyncio.sleep(3)
+
+async def async_generator(num):
+    for i in range(num):
+        await asyncio.sleep(1)
+        yield i
+
+
 async def main():
-    task1 = asyncio.create_task(task_1())
-    task2 = asyncio.create_task(task_2())
-    await asyncio.gather(task1, task2)
+    async for value in async_generator(3):
+        print(value)
+
+
 asyncio.run(main())
