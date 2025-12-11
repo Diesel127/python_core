bc59576058e462b46f48adbe6884fa5b86d5c71e
Diesel127 <retina127127@gmail.com>
2025-12-11 12:08:41 +0100

    lesson35

--- PythonProject3/main.py
+++ PythonProject3/main.py
@@ -1,13 +1,13 @@
 import asyncio
 
-
-def my_callback(*args, **kwargs):
-    print(loop.is_running())
-    print("Callback processed")
-    loop.stop()
-
-
-loop = asyncio.get_event_loop()
-loop.call_later(3, my_callback, loop)
-loop.run_forever()
-print(loop.is_running())
+async def task_1():
+    print("Первая задача")
+    await asyncio.sleep(2)
+async def task_2():
+    print("Вторая задача")
+    await asyncio.sleep(3)
+async def main():
+    task1 = asyncio.create_task(task_1())
+    task2 = asyncio.create_task(task_2())
+    await asyncio.gather(task1, task2)
+asyncio.run(main())
