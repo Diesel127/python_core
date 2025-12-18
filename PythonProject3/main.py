4b49486511baf462ac97064b007cbf802066e04a
Diesel127 <retina127127@gmail.com>
2025-12-18 20:25:09 +0100

    lesson38

--- PythonProject3/main.py
+++ PythonProject3/main.py
@@ -1,14 +1,9 @@
-def plusOne(digits):
-    for i in range(len(digits) - 1, -1, -1):
-        if digits[i] < 9:
-            digits[i] += 1
-            return digits
-        digits[i] = 0
-    return [1] + digits
+def my_sqrt(num: int) -> bool:
+    odd = 1
+    while num > 0:
+        num -= odd
+        odd += 2
+    return num == 0
 
 
-print(plusOne([9]))
-print(plusOne([9,9,9,9,9]))
-print(plusOne([1,2,3]))
-print(plusOne([1,2,9]))
-print(plusOne([8,9,9,9]))
+print(my_sqrt(5))
